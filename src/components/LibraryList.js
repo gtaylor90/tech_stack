f9a91b4eee59a8b05e032afac64a1b9c15e04257
gtaylor90 <graham.taylor.90@gmail.com>
2017-04-29 17:57:18 -0500

    got a little hairy there but we okay

--- src/components/LibraryList.js
+++ src/components/LibraryList.js
@@ -1,15 +1,15 @@
 import React, { Component } from 'react';
 import { ListView } from 'react-native';
 import { connect } from 'react-redux';
-import ListItem from './components/ListItem';
+import ListItem from './ListItem';
 
 class LibraryList extends Component {
   componentWillMount() {
     const ds = new ListView.DataSource({
-      rowHasChanged: () => (r1, r2) => r1 !== r2
+      rowHasChanged: (r1, r2) => r1 !== r2
     });
 
-    this.dataSource = ds.cloneWithRows(this.props.libaries);
+    this.dataSource = ds.cloneWithRows(this.props.libraries);
   }
 
   renderRow(library) {
--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -1,8 +1,16 @@
 import React, { Component } from 'react';
+import { Text } from 'react-native';
+import { CardSection } from './common';
 
 class ListItem extends Component {
-  render(){
-    return;
+  render() {
+    return (
+      <CardSection>
+        <Text>
+          {this.props.library.title}
+        </Text>
+      </CardSection>
+    );
   }
 }
 
