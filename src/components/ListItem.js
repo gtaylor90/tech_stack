8faa8b3fb6c4c73c4e047370e92da33b378c5610
gtaylor90 <graham.taylor.90@gmail.com>
2017-05-19 12:43:14 -0500

    styling

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -4,14 +4,23 @@ import { CardSection } from './common';
 
 class ListItem extends Component {
   render() {
+    const { titleStyle } = styles;
     return (
       <CardSection>
-        <Text>
+        <Text style={titleStyle}>
           {this.props.library.title}
         </Text>
       </CardSection>
     );
   }
 }
+
+const styles = {
+  titleStyle: {
+    fontSize: 18,
+    paddingLeft: 15
+  }
+};
+
 
 export default ListItem;
